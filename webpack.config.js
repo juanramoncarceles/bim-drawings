4728bef056ccd10b97037dab384581c88e5c0a6d
Ramon Carceles <juanramoncarceles@gmail.com>
2020-06-26 20:08:14 +0200

    Edit scripts to include mode variable.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
+require('dotenv').config();
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -46,6 +47,14 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./index.html",
       favicon: './favicon.ico'
+    }),
+    new webpack.DefinePlugin({
+      PROCESS: JSON.stringify(process.env.PROCESS),
+      "process.env": {
+        GOOGLE_CLIENT_ID: JSON.stringify(process.env.GOOGLE_CLIENT_ID),
+        GOOGLE_API_KEY_DEV: JSON.stringify(process.env.GOOGLE_API_KEY_DEV),
+        GOOGLE_API_KEY_PROD: JSON.stringify(process.env.GOOGLE_API_KEY_PROD)
+      }
     })
   ]
 };
